6f0d666becc174f42cce6c870eebd887d4632138
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-07-28 15:35:28 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+type Product struct {
+	Name     string `json:"name"`
+	Price    string `json:"price"`
+	ImageUrl string `json:"imageUrl"`
+}
+
 func main() {
-	
+	c := colly.NewCollector(
+		colly.AllowedDomains("jumia.com.ng"),
+	)
+
+	fmt.Println("about to")
+
+	c.OnHTML("a.core", func(h *colly.HTMLElement) {
+		fmt.Println("=======")
+		fmt.Println(h.Text)
+	})
+
+	c.Visit("https://www.jumia.com.ng")
 }
